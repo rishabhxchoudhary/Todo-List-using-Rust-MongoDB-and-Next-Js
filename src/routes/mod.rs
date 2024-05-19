bf6530dd1eb9d7e089dc653f44142814f5a36cf7
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2024-05-20 01:50:47 +0530

    allow cors

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,11 +1,13 @@
 
 use axum::extract::Path;
+use axum::http::Method;
 use axum::routing::{delete, patch};
 use axum::Json;
 use axum::{routing::{get,post}, Router, extract::State};
 use crate::models::models::Todo;
 use crate::state::AppState;
 use crate::controllers::controllers;
+use tower_http::cors::{Any, CorsLayer};
 
 async fn get_all_tasks(State(state) : State<AppState>) -> Json<Vec<Todo>> {
     let tasks: Vec<Todo> = controllers::get_all_tasks(state).await;
@@ -38,6 +40,10 @@ async fn toggle_task(State(state) : State<AppState>, Path(id) : Path<String>) {
 }
 
 pub fn create_route(app_state: AppState) -> Router<()> {
+    let cors = CorsLayer::new()
+    .allow_methods([Method::GET, Method::POST])
+    .allow_origin(Any);
+
     Router::new()
     .route("/", get(get_all_tasks))
     .route("/todo/:id", get(get_task_by_id))
@@ -45,4 +51,5 @@ pub fn create_route(app_state: AppState) -> Router<()> {
     .route("/todo/:id", delete(delete_task))
     .route("/todo/toggle/:id", patch(toggle_task))
     .with_state(app_state)
+    .layer(cors)
 }
